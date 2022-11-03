25034e70c2970f5fc20719aa13e11ff17a693a47
JULIETHRODRIGUEZ14 <jarodriguezalejitta3275@misena.edu.co>
2022-11-03 11:54:16 -0500

    Consola interactiva

---
+++ app2.js
@@ -0,0 +1,57 @@
+require('colors');
+   console.log('=================================================================='.red)
+    console.log('|                     Iniciar sesion                            |'.red)
+    console.log('|                      ⣀⣠⣤⣤⣤⣤⣤⣤⣤⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀             |'.red)
+    console.log('|                 ⣀⡴⠶⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠛⠷⢦⣄⡀                         |'.red)   
+    console.log('|    ⠀⠀⠀ ⠀ ⠀⠀⠀⠀⣰⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀              |'.red)
+    console.log('|    ⠀⠀  ⣴⠶⣤⣀⠀⣼⠋⠀⠀⠀⠀⠀⣠⡶⠛⢷⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀              |'.red)
+    console.log('|      ⠀⢸⠃⠀⠀⣙⣷⠷⠶⠖⠲⠶⠦⣾⣏⠀⠀⠀⣷⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣠⣤⠤⠸⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀              |'.red)
+    console.log('|      ⠀⢸⢀⡴⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⢦⡀⣿⠀⠉⠉⠉⠉⠉⠉⢉⣀⣀⣀⣤⣤⣤⣽⠂⠀⠀⠀⠀⠀⠀⠀⠀              |'.red)
+    console.log('|     ⠀⢸⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡟⠛⠛⠛⠛⠛⠛⠛⠉⠁⠉⠀⠀⠀⠀⠹⣇⠀⠀⠀⠀⠀⠀⠀⠀               | '.red)
+    console.log('|    ⢀⡿⠀⢠⣤⡄⠀⠀⠀⣄⠀⠀⠀⢴⣶⡄⠀⠀⢻⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡿⠀⠀⠀⠀⢀⣴⠟⠻⡆               |'.red)
+    console.log('|    ⢾⣧⠀⠈⠛⠁⠀⠰⠶⠿⠶⠖⠀⠈⠉⠀⠀⠀⣸⣟⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣾⣁⣀⣀⣀⡴⠟⠁⠀⣼⠇               |'.red)
+    console.log('|    ⠒⢻⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⡿⢭⣥⠤⢤⡴⠶⠶⠖⠒⠚⠛⠛⠉⣻⣆⡉⠉⠉⢁⣀⣠⡴⠟⠁⠀               | '.red)
+    console.log('|    ⠀⠀⠙⢷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡾⠋⠀⢤⣤⣤⠤⠤⠤⠤⠤⠶⠶⣺⡴⠞⣿⠈⠉⠉⠉⠉⠉⠀⠀⠀⠀⠀               |'.red)
+    console.log('|    ⠀⠀⠀⠀⠉⠛⠶⠤⣤⣤⣤⣤⡴⠶⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡾⠃⠀⢠⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀               |'.red)
+    console.log('|    ⠀⠀⠀⠀⠀⠀⠀⢀⡿⠙⠳⢦⣤⣀⣀⠀⠀⡶⠛⠙⢳⣄⣀⣀⣤⣴⡶⠛⣧⠀⠀⣸⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀               |'.red)
+    console.log('|    ⠀⠀⠀⠀⠀⠀⢀⣾⠃⠀⢀⡾⠋⠉⠉⠙⠛⣇⠀⠀⣸⠏⣹⠁⠀⣸⠃⢀⣏⣀⣠⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                |'.red)
+    console.log('|   ⠀⠀⠀⠀⠀⠀⠺⣷⣶⡶⠟⠁⠀⠀⠀⠀⢀⣿⣤⣤⡟⠀⣿⣶⣶⠏⠀⠘⠻⠟⠋                             | ' .red)
+    console.log('=================================================================='.red)
+    const preguntas = [
+      {
+          type: 'list',
+          name: 'opcion',
+          message: '¿Qué desea hacer?',
+          choices: [
+              {
+                  value: '1',
+                  name: `${ '1.'.brightGreen } Iniciar sesion`
+              },
+              {
+                  value: '2',
+                  name: `${ '2.'.brightGreen } Ver tareas`
+              },
+              {
+                  value: '3',
+                  name: `${ '3.'.brightGreen } Ver tareas completadas`
+              },
+              {
+                  value: '4',
+                  name: `${ '4.'.brightGreen } Ver tareas pendientes`
+              },
+              {
+                  value: '5',
+                  name: `${ '5.'.brightGreen } Completar tarea(s)`
+              },
+              {
+                  value: '6',
+                  name: `${ '6.'.brightGreen } Borrar tarea`
+              },
+              {
+                  value: '0',
+                  name: `${ '0.'.brightGreen } Salir`
+              },
+              
+          ]
+      }
+  ];
